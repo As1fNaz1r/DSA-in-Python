8807012876df4fca635e6eb04d8e7ab5a2f125d9
Asif Nazir <asifnazirbhat313@gmail.com>
2026-03-22 10:17:36 +0530

    Remove duplicate elements from the sorted array

---
+++ Remove Duplicates from Sorted Array.py
@@ -0,0 +1,19 @@
+import typing as tp
+class Solution:
+    def removeDuplicates(self, nums: tp.List[int]) -> int:
+        if not nums:
+            return 0
+        i = 1
+        for j in range(2, len(nums)):
+            if nums[j] != nums[i]:
+                i += 1
+                nums[i] = nums[j]
+        return i+1
+
+
+
+
+nums = [1, 2, 2, 2, 3, 4, 4, 5]
+solution = Solution()
+k = solution.removeDuplicates(nums)
+print(k)
